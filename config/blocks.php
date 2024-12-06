a49b3e5f52c879ef1e23b9199efd7306f9712046
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-12-06 10:22:52 +0100

    (chore) update for phpstan

--- config/blocks.php
+++ config/blocks.php
@@ -9,13 +9,13 @@ return [
      * @see https://developer.wordpress.org/reference/functions/register_block_type/
      */
     'client-side-block' => [
-        'blockType' => 'htdocs/theme/block',
+        'blockType' => asset('client-side-block')->path(),
         'args' => [],
     ],
     'server-side-block' => [
-        'blockType' => 'theme/block',
+        'blockType' => asset('server-side-block')->path(),
         'args' => [
-            'render_callback' => 'renderCallback',
+            'render_callback' => '__return_empty_string',
         ],
     ],
 ];
