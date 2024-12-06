f64e0a1189c287b6520a5151819b86309d53639e
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-12-06 10:22:52 +0100

    (chore) update for phpstan

--- resources/views/blocksregistration.blade.php
+++
@@ -1,1 +0,0 @@
-{{ BlocksRegistration::getQuote() }}
--- src/Registrar.php
+++ src/Registrar.php
@@ -8,15 +8,27 @@ class Registrar
 {
     public function __construct()
     {
-        add_action('init', $this->registerBlocks());
+        add_action('init', $this->registerBlocks(...));
     }
 
     public function registerBlocks(): void
     {
+        /** @var array<string, array<string,mixed>> */
         $blocks = config('blocks');
 
         foreach ($blocks as $block) {
-            \register_block_type($block['blockType'], $block['args']);
+            /** @var string|\WP_Block_Type */
+            $blockType = $block['blockType'];
+            /** @var array<string,mixed> */
+            $blockArgs = $block['args'];
+
+            $args = new \Args\WP_Block_Type();
+            $args->fromArray($blockArgs);
+
+
+            // @phpstan-ignore argument.type
+            \register_block_type($blockType, $args->toArray());
+
         }
     }
 }
