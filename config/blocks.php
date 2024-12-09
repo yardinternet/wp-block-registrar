4bbf8468dbc9898d86f0e8e2898fcca542edafd0
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-12-09 10:50:47 +0100

    (chore) fix config

--- config/blocks.php
+++ config/blocks.php
@@ -8,14 +8,14 @@ return [
      *
      * @see https://developer.wordpress.org/reference/functions/register_block_type/
      */
-    'client-side-block' => [
-        'blockType' => asset('client-side-block')->path(),
-        'args' => [],
-    ],
-    'server-side-block' => [
-        'blockType' => asset('server-side-block')->path(),
-        'args' => [
-            'render_callback' => '__return_empty_string',
-        ],
-    ],
+    // 'client-side-block' => [
+    //     'blockType' => asset('client-side-block/block.json')->path(),
+    //     'args' => [],
+    // ],
+    // 'server-side-block' => [
+    //     'blockType' => asset('server-side-block/block.json')->path(),
+    //     'args' => [
+    //         'render_callback' => '__return_empty_string',
+    //     ],
+    // ],
 ];
